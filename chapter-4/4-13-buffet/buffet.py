30b6ca1b50760f96e91fa507963320585a9db030
Senbonzakur4 <ozielvelazquez24@outlook.com>
2026-07-25 20:27:13 -0600

    feat: add Chapter 4 excercise

---
+++ chapter-4/4-13-buffet/buffet.py
@@ -0,0 +1,18 @@
+# 3.13. Buffet
+
+menu = ('Garlic Bread', 'Beef Burger', 'Caesar Salad', 'Pizza', 'Beef Steak')
+
+print()
+
+for food in menu:
+    print(food)
+
+menu = ('Garlic Bread', 'Beef Burger', 'Caesar Salad', 'Pasta', 'Chicken Wings')
+
+print()
+
+for food in menu:
+    print(food)
+
+print()
+      
